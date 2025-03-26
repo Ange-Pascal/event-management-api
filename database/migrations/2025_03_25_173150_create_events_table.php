1f249ac76706e268afcb7072d6d1ad977e34575f
Ange-Pascal <angeakoumoua@gmail.com>
2025-03-26 09:35:30 +0100

    second commit avec la route api et model crée

--- database/migrations/2025_03_25_173150_create_events_table.php
+++ database/migrations/2025_03_25_173150_create_events_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('events', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
+            $table->foreignIdFor(User::class); 
+            $table->string('name'); 
+            $table->text('description')->nullable(); 
+            
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
             $table->timestamps();
         });
     }
